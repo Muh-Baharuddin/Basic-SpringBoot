67fd7960ddd0dea92d4ee503938d1521d2da3dd3
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-11-20 18:40:48 +0700

    bean post processor

--- src/main/java/com/practice/basic/beanPostProcessor/aware/IdAware.java
+++ src/main/java/com/practice/basic/beanPostProcessor/aware/IdAware.java
@@ -3,4 +3,6 @@ package com.practice.basic.beanPostProcessor.aware;
 public interface IdAware {
   
   void setId(String id);
+
+  String getId();
 }
--- src/main/java/com/practice/basic/beanPostProcessor/processor/IdGeneratorBeanPostProcessor.java
+++ src/main/java/com/practice/basic/beanPostProcessor/processor/IdGeneratorBeanPostProcessor.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
 import com.practice.basic.beanPostProcessor.aware.IdAware;
@@ -12,8 +13,13 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
-public class IdGeneratorBeanPostProcessor implements BeanPostProcessor {
-  
+public class IdGeneratorBeanPostProcessor implements BeanPostProcessor, Ordered {
+  @Override
+  public int getOrder() {
+    return 1;
+  }
+
+
   @Override
   public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
     log.info("id generator processor for Bean {}", beanName);
